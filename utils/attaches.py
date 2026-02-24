d2f4e3d827de1c3698e39f121bd05c6ca8b16b86
Seyfullah Ismailov <seyfullahismailly@gmail.com>
2026-02-24 19:55:34 +0300

    remade attaches.py correctly

--- utils/attaches.py
+++ utils/attaches.py
@@ -9,9 +9,9 @@ def add_screenshot(driver):
 
 def add_logs(driver):
     log = "".join(f'{text}\n' for text in driver.get_log(log_type='browser'))
-    allure.attach(body=log, name="browser_logs", AttachmentType=AttachmentType.TEXT, extension='.log')
+    allure.attach(body=log, name="browser_logs", attachment_type=AttachmentType.TEXT, extension='.log')
 
 
 def add_html(driver):
     html = driver.page_source
-    allure.attach(body=html, name='page_source', AttachmentType=AttachmentType.HTML, extension='.html')
+    allure.attach(body=html, name='page_source', attachment_type=AttachmentType.HTML, extension='.html')
